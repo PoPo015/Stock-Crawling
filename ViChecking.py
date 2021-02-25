4a15d6973a4990e02acc4c70bbe0226a8e7d65b3
Youngpro95 <mmyoung95@naver.com>
2021-02-25 17:49:53 +0900

    '0225_modifiy'

--- ViChecking.py
+++ ViChecking.py
@@ -7,37 +7,37 @@ nowDate = now.strftime('%Y년 %m월 %d일 %H시 %M분 입니다.')
 print(nowDate)
 
 chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless') #백그라운드화
+chrome_options.add_argument('window-size=1920x1080') #윈도우 사이즈 크기 조절
 driver = webdriver.Chrome(chrome_options=chrome_options) #웹은 크롬&옵션사용
 driver.maximize_window()
-driver.get('http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC02021501')
-time.sleep(0.5)
+driver.get('http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC02021501') #크롤링할 url
+time.sleep(1)
 full = driver.find_element_by_xpath('//*[@id="jsViewSizeButton"]').click() #전체화면 vi해제시각 까지 불러오려고
-driver.find_element_by_xpath('//*[@id="jsMdiContent"]/div/div[1]/div[1]/div[1]/div[1]/div/div/table/thead/tr[1]/td[9]/div/div/a').click()
-driver.find_element_by_xpath('//*[@id="jsMdiContent"]/div/div[1]/div[1]/div[1]/div[1]/div/div/table/thead/tr[1]/td[9]/div/div/a').click()
+driver.find_element_by_xpath('//*[@id="jsMdiContent"]/div/div[1]/div[1]/div[1]/div[1]/div/div/table/thead/tr[1]/td[9]/div/div/a').click() #sort 버튼 클릭
+driver.find_element_by_xpath('//*[@id="jsMdiContent"]/div/div[1]/div[1]/div[1]/div[1]/div/div/table/thead/tr[1]/td[9]/div/div/a').click() #sort 버튼 클릭
 vi_result = [] #결과값 담을 배열
-final_vi_result=[]
 
 def inner_scroll():
-    scroll_moving = driver.find_elements_by_class_name('CI-FREEZE-SCROLLER') #여기에 scroll
-    sc_test = driver.find_element_by_class_name('CI-FREEZE-SCROLLER-INNER')
-    scroll_height = sc_test.size['height']
+    scroll_bar = driver.find_element_by_class_name('CI-FREEZE-SCROLLER-INNER')
+    scroll_height = scroll_bar.size['height']
     last_index = driver.find_element_by_xpath('//*[@id="jsMdiContent"]/div/div[1]/div[1]/div[2]')
     scroll_range = 540
     while True:
 
         try:
-            driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight=' + str(scroll_range) + '',last_index)
+            driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight=' + str(scroll_range) + '', last_index)
             scroll_range += 540
         except:
             print("실패")
         show_VI()
         if scroll_range >= scroll_height: #총 스크롤길이 넘어가면 멈춤
             break
-    print("끝")
+    print("스크롤링 끝")
+
 def show_VI():
-    vi_body = driver.find_element_by_xpath('//*[@id="jsMdiContent"]/div/div[1]/div[1]/div[1]/div[2]/div/div/table/tbody').text
-    transfer_list = vi_body.split("\n")
+    vi_body = driver.find_element_by_xpath('//*[@id="jsMdiContent"]/div/div[1]/div[1]/div[1]/div[2]/div/div/table/tbody').text #텍스트 다 긁어오기
+    transfer_list = vi_body.split("\n") #\n 개행문자로 나누기
     vi_result.extend(transfer_list) # append 를 안한 이유는 텍스트를 리스트로 바꾸는 작업을 하기때문에 append가아닌 extend로 확장시킴
 
 def check_overlap():
@@ -48,22 +48,49 @@ def check_overlap():
             count += 1
             # print(count)
             vi_result.remove(i) # 중복값 제거
+
 def setting_vi():
-    test_list =[]
+    stock_info_array = []
+    convertJson_array = []
+    convertJson_dic = {}
+
+    count = 0
+    start = 1
+
     for i in range(len(vi_result)):
-        test = vi_result[i].split()
-        test_list.extend(test) #11개로 나뉨
-    print(test_list)
-0
-{
-    번호 : 1
-    종목코드 : 100700
-    종목이름 : 세운메디칼
-    가격 : 8910
-    상승률 : 18.30
-    발동시각 : 09:00:05
-    해제시각 : 09:02:18
-}
+        vi_split = vi_result[i].split() # 공백으로 나눔
+        stock_info_array.extend(vi_split) #각 배열에 11개로 저장
+        stk_id = stock_info_array[0 + count] #번호
+        stk_cd = stock_info_array[2 + count] #종목코드
+        stk_nm = stock_info_array[3 + count] #종목이름
+        if stock_info_array[4+count] != "KOSPI": #띄어쓰기되어 있는 종목 이름 붙이기
+            if stock_info_array[4+count] != "KOSDAQ":
+                stk_nm = stock_info_array[3 + count]+" "+stock_info_array[4+count]
+                del stock_info_array[4+count]
+        stk_pri = stock_info_array[6 + count] #가격
+        stk_inc = stock_info_array[7 + count] #상승률
+        stk_act = stock_info_array[9 + count] #발동시각
+
+        try:
+            stk_rel = stock_info_array[10 + count] #해제시각
+        except:
+            stock_info_array.append("00:00:00")
+            stk_rel = '00:00:00'
+            print(stk_nm+" 종목 해제시각 null 대신 00:00:00 입력")
+
+        result_dic  = {'stk_id':stk_id, 'stk_cd':stk_cd, 'stk_nm':stk_nm, 'stk_pri':stk_pri, 'stk_inc':stk_inc, 'stk_act':stk_act, 'stk_rel':stk_rel} # 딕셔너리 key:Value 값 입력
+        convertJson_array.append(result_dic) #딕셔너리를 배열에 추가
+        convertJson_dic = {'date_list': convertJson_array}  #다시 배열을 딕셔너리로 다중 key:value를 이용하기 위하여
+        count += 11
+        start += 1
+    print("json 저장중")
+    with open('vi_data.json', 'w', encoding="utf-8") as f: # json 파일 저장 현재경로에
+        json.dump(convertJson_dic, f, ensure_ascii=False, indent="\t")
+
+
+
+
+
 show_VI()
 inner_scroll()
 check_overlap()
