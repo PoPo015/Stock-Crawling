09045805998eb6b599eaec8598a8a43b8558d687
Youngpro95 <44990343+Youngpro95@users.noreply.github.com>
2021-03-17 04:33:07 +0900

    ~일 전, ~시간 전, ~분 전 만 보낼 수 있도록 수정, 최근 일주일 간 뉴스만 크롤링

--- naver_test.py
+++ naver_test.py
@@ -1,36 +1,52 @@
 from bs4 import BeautifulSoup
 import requests,datetime,time
 import json
+import logging
+logging.basicConfig(
+    filename='./py_naver_saerch_warning.log',
+    format= '%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)s] >> %(message)s',
+    datefmt= '%m/%d/%Y %I:%M:%S %p',
+    level = logging.ERROR
+)
+
 
 def news_search():
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; ko-KR))',} #안티 크롤링
-    url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query="+keyword
-    req = requests.get(url.format(keyword))
-    soup = BeautifulSoup(req.text, 'html.parser')
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; ko-KR))',} #안티 크롤링
+        url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query="+keyword
+        req = requests.get(url.format(keyword))
+        soup = BeautifulSoup(req.text, 'html.parser')
 
-    news_dict = {}
-    data = []
+        news_dict = {}
+        data = []
 
-    table = soup.select('#main_pack > section > div > div.group_news > ul > li')
-    for i in table:
-        news_title = i.find(class_='news_tit').text
-        news_href = i.find(class_='news_tit')['href']
-        news_time = i.find("span", {"class": "info"}).text
-        print(news_title + " " + news_time)
-        print(news_href)
-        news_dict = {'news_company':keyword, 'news_title': news_title, 'news_time': news_time, 'news_href': news_href}
-        data.append(news_dict) #배열 추가
-        news_dict = {'news_Data':data}
+        table = soup.select('#main_pack > section > div > div.group_news > ul > li')
+        for i in table:
+            news_title = i.find(class_='news_tit').text
+            news_href = i.find(class_='news_tit')['href']
+            news_time = i.find("span", {"class": "info"}).text
+            word_filter = ['일', '시간', '분']
+            for x in word_filter:
+                if x in news_time:
+                    news_dict = {'news_company': keyword, 'news_title': news_title, 'news_time': news_time,'news_href': news_href}
+                    data.append(news_dict)  # 배열 추가
+                    news_dict = {'news_Data': data}
         with open('news_search_data.json', 'w', encoding="utf-8") as f:  # json 파일 저장 현재경로에
             json.dump(news_dict, f, ensure_ascii=False, indent="\t")
-        with open('news_search_data.json', 'r', encoding='utf-8') as json_result:  # json 파일 load
-            json_data = json.load(json_result)
-    send_json = json.dumps(news_dict, indent='\t')
-    url = 'http://localhost:8081/stock/viNewsReg'
-    headers = {'Content-Type': 'application/json; charset=utf-8'}
-    requests.post(url=url, data=send_json, headers=headers)
+        send_json = json.dumps(news_dict, indent='\t')
+        url = 'http://localhost:8081/stock/VI_NewsReg'
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        requests.post(url=url, data=send_json, headers=headers)
+    except Exception as e:
+        logging.error("ERROR : %s", e)
+
 
 while True:
+    logger = logging.getLogger('simple_example')
+    logger.setLevel(logging.DEBUG)
+    fh=logging.FileHandler(filename='testlog.log')
+    fh.setLevel(logging.INFO)
+    # logging.basicConfig(filename='logfile.log', level=logging.ERROR)
     with open('vi_data.json', 'r', encoding='utf-8') as json_result:  # json 파일 load
         try:
             json_data = json.load(json_result)
@@ -51,10 +67,7 @@ while True:
         trans_time = '%H:%M:%S'
         time_cal = now.strptime(nowtime, trans_time) - datetime.datetime.strptime(vi_time, trans_time)
 
-        if int(time_cal.seconds) < 60:
+        if int(time_cal.seconds) < 120:
             print("시간계산: " + str(time_cal.seconds)) #시간초 단위로 변환
             print("크롤링 완료")
             news_search()
-
-
-
